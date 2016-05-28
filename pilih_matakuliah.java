996711134d759c76eddc6cf5bfbecc7df9b6a8b2
Alexander Arie Dharma Putra <xerozero79@gmail.com>
2016-05-28 11:27:11 +0700

    Add files via upload
    class untuk memilih mata kuliah

---
+++ pilih_matakuliah.java
@@ -0,0 +1,21 @@
+package SIM;
+public class pilih_matakuliah {
+    private mahasiswa nim;
+    private matakuliah kode_mk;
+
+    public mahasiswa getNim() {
+        return nim;
+    }
+
+    public void setNim(mahasiswa nim) {
+        this.nim = nim;
+    }
+
+    public matakuliah getKode_mk() {
+        return kode_mk;
+    }
+
+    public void setKode_mk(matakuliah kode_mk) {
+        this.kode_mk = kode_mk;
+    }
+}
